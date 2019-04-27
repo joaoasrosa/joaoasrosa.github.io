de7aaa843216b38bf7a7e490e83cff7f11bfc107
João Rosa <joaoasrosa@gmail.com>
2019-04-27 14:26:53 +0200

    Migrate website

--- Gemfile
+++ Gemfile
@@ -4,3 +4,7 @@ gem 'github-pages', '>= 155', group: :jekyll_plugins
 gem "jekyll-include-cache", "~> 0.2.0"
 
 gem "jekyll-import", "~> 0.18.1"
+
+gem "hpricot", "~> 0.8.6"
+
+gem "open_uri_redirections", "~> 0.2.1"
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,111 @@
 var store = [{
-        "title": "Test",
-        "excerpt":"Why  Some text.   Italics   BOLD  ","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/2017/08/24/test.html",
+        "title": "My CISSP journey",
+        "excerpt":"In this post I'm writing about my CISSP journey. For those who aren't aware of what CISSP stand for, it's (probably) the most respectful security certification. It's under (ISC)2 portfolio and you can find more information here. My journey started in November&nbsp;2015, when I asked myself \"What about now?\". Back...","categories": ["Security","Training"],
+        "tags": ["Certification","CISSP","ISC2"],
+        "url": "http://localhost:4000/2016/05/22/my-cissp-journey/",
+        "teaser":null},{
+        "title": "Update project.json version number in AppVeyor",
+        "excerpt":"Today was a debut using GitHub Gist. In one of my GitHub projects, I need a close integration between the Continuous Integration tool (in this case AppVeyor) and my .NET Core project, specifically in how to update the version of the DLL based on the version in AppVeyor? To sort out...","categories": ["Continuous Integration","OSS"],
+        "tags": ["AppVeyor","Gist","GitHub","PowerShell"],
+        "url": "http://localhost:4000/2016/09/14/update-project-json-version-number-in-appveyor/",
+        "teaser":null},{
+        "title": "Integrate .NET Core, AppVeyor, OpenCover and Codecov",
+        "excerpt":"As a developer, I need to take a good rest at night. In my line of work the only way to achieve it is using an integrated workflow pipeline, aka Source Code Management (SCM), Continuous Integration (CI) and Continuous Deployment (CD) with some tools in the middle. For my new pet...","categories": ["Continuous Deployment","Continuous Integration","OSS"],
+        "tags": ["AppVeyor","Code Coverage","Codecov","Gist","GitHub","NuGet","OpenCover","PowerShell","Testing"],
+        "url": "http://localhost:4000/2016/10/12/integrate-net-core-appveyor-opencover-and-codecov/",
+        "teaser":null},{
+        "title": "Changing the operations of a Software Company - Part I",
+        "excerpt":"Agile is great... and sometimes painful. Do not get me wrong, I love an Agile work environment when everyone is keen to \"Try, Fail, Fix, Learn and Move On\". For those ones that work in the software development space, we have more tasks than develop software. One of the tasks,...","categories": ["Continuous Deployment","Continuous Integration"],
+        "tags": [".NET","Deployment","DevOps","Jenkins","nant","Octopus","PowerShell","Testing"],
+        "url": "http://localhost:4000/2016/12/02/changing-the-operations-of-a-software-company-part-i/",
+        "teaser":null},{
+        "title": "Changing the operations of a Software Company – Part II",
+        "excerpt":"In the previous post, I wrote about my experience changing the operations of a Software Company, improving the development process. After we tackle this problem, we move to the next one: How can we bring the automation to our client installer? To give some context, the software is installed on client...","categories": ["Continuous Deployment","Continuous Integration"],
+        "tags": ["Deployment","DevOps","Octopus"],
+        "url": "http://localhost:4000/2016/12/06/changing-the-operations-of-a-software-company-part-ii/",
+        "teaser":null},{
+        "title": "Running Docker SQL Server Linux container in Windows 10",
+        "excerpt":"Microsoft is doing a huge effort to bring SQL Server 2016 to Linux. Back in the summer, during the Dublin 2016 SQL Saturday #501, Mark Souza presented the SQL Server history. One of the major features is the ability to run SQL Sever 2016 on Linux. Since then I added...","categories": ["SQL Server"],
+        "tags": ["Docker","Linux","Windows 10"],
+        "url": "http://localhost:4000/2016/12/21/running-docker-sql-server-linux-container-in-windows-10/",
+        "teaser":null},{
+        "title": "Code development improvement with JetBrains UpSource (configuration and first impressions)",
+        "excerpt":"We should seek excellence in our jobs, doesn't matter our line of work. As professionals, we should always aim to improve the quality of our outputs, either it is customer satisfaction, code quality or just faster builds and deployments. As any developer knows, working with a Version Control System (VCS) is...","categories": ["Code Development","Continuous Deployment","Continuous Integration"],
+        "tags": ["CentOS 7","Continuous Improvement","Continuous Innovation","JetBrains","Linux","UpSource","VCS","Visual Studio"],
+        "url": "http://localhost:4000/2017/01/03/code-development-improvement-with-jetbrains-upsource-configuration-and-first-impressions/",
+        "teaser":null},{
+        "title": "Create an AWS Lambda function from an Octopus Deployment",
+        "excerpt":"AWS Lambda is one of the AWS Compute Services, alongside with Amazon EC2 or Amazon EC2 Container Service. In a nutshell, AWS Lambda is the serverless offer from AWS, allowing \"you run code without provisioning or managing servers\"[1]. In the DevOps spirit, and using Octopus as the Continuous Deployment tool, I couldn't find a step...","categories": ["Cloud Computing","Continuous Deployment","Continuous Integration","OSS"],
+        "tags": [".NET",".NET Core","AWS","AWS Lambda","Deployment","DevOps","GitHub","NuGet","Octopus","PowerShell","Visual Studio"],
+        "url": "http://localhost:4000/2017/01/10/create-an-aws-lambda-function-from-an-octopus-deployment/",
+        "teaser":null},{
+        "title": "Create and Push a Docker Image from an Octopus Deployment",
+        "excerpt":"Docker is one of the disruptive technologies within virtualization, allowing the different containers to run on the same machine, sharing resources, reducing the overhead. The technology allows DevOps teams to have another tool to develop, build and ship software. One use case for containers is the Microservices Architecture Pattern. Background...","categories": ["Continuous Deployment","Continuous Integration","OSS"],
+        "tags": [".NET",".NET Core","Continuous Improvement","Continuous Innovation","Deployment","DevOps","Docker","GitHub","Octopus","PowerShell","VCS"],
+        "url": "http://localhost:4000/2017/02/04/create-and-push-a-docker-image-from-an-octopus-deployment/",
+        "teaser":null},{
+        "title": "Build a .NET Core 2.0 Application in AppVeyor",
+        "excerpt":"AppVeyor is one of the Continuous Integration Services in the cloud, ready to use, with minimal configuration. It provides a Windows Server image with tooling to build, test and deploy ours .NET applications. Among the baseline software is .NET Core, however, the version is 1.0.4 (at the time of writing this...","categories": ["Continuous Integration","OSS"],
+        "tags": [".NET Core","AppVeyor","Gist","GitHub","PowerShell"],
+        "url": "http://localhost:4000/2017/08/10/build-a-net-core-2-0-application-in-appveyor/",
+        "teaser":null},{
+        "title": "NDepend, first impressions on a static code analyser",
+        "excerpt":"During my career, I learned on the hardest way how a poor codebase can be harmful to the business. I witness behaviours in teams such as: Missing code reviews. How can we be a team, if we do not communicate? No coding standards. At least settle the capitalisation style? Testing. Wait, spending time...","categories": ["Code Development","Continuous Integration"],
+        "tags": [".NET",".NET Core","Code Coverage","Continuous Improvement","Continuous Innovation","DevOps","NDepend","Testing","VCS","Visual Studio"],
+        "url": "http://localhost:4000/2017/10/01/ndepend-first-impressions-on-a-static-code-analyser/",
+        "teaser":null},{
+        "title": "Abstract from your CI. Use Cake!",
+        "excerpt":"Here at Coolblue, we can choose our tools from a range of technologies. A while ago, our team started moving from Microsoft MSTest to xUnit. Although the code change was fairly simple, the build pipeline was tied with the MSTest framework, ignoring the xUnit tests. Between the requests to the...","categories": ["Code Development","Continuous Deployment","Continuous Integration"],
+        "tags": [".NET",".NET Core","Applications","AppVeyor","Cake","Continuous Improvement","Continuous Innovation","DevOps","Docker","Jenkins","Linux","TravisCI","Windows"],
+        "url": "http://localhost:4000/2017/10/18/abstract-from-your-ci-use-cake/",
+        "teaser":null},{
+        "title": "My first Coolblue Hackathon. How to be hacked",
+        "excerpt":"Last week we had our annual Hackathon, my first at Coolblue. Here we are taken to a remote location for 3 days to hack away. I joined a team who wants to build an application to measure the happiness in Coolblue. Wait, measure happiness? How? It's a straightforward concept: after...","categories": ["Code Development"],
+        "tags": ["Hackathon"],
+        "url": "http://localhost:4000/2017/10/22/my-first-coolblue-hackathon-how-to-be-hacked/",
+        "teaser":null},{
+        "title": "TechDays NL 2017",
+        "excerpt":"The first conference that I attend since we arrived in The Netherlands was the TechDays NL 2017. The conference is sponsored by Microsoft, and it's one of the most significant tech events in the Benelux area. However, even being sponsored by Microsoft, other streams of the industry were present, showing...","categories": ["Conferences"],
+        "tags": [".NET",".NET Core","Applications","Continuous Improvement","Deployment","Docker","GitHub","JetBrains","Linux","NuGet","PowerShell","Testing","Visual Studio","Windows"],
+        "url": "http://localhost:4000/2017/12/09/techdays-nl-2017/",
+        "teaser":null},{
+        "title": "Learning DDD as a team",
+        "excerpt":"A few sprints ago my team and I decided to invest heavily in Domain Driven Desing. We already started to develop the new microservices in a DDD fashion, with our domain as the central component. However, we wanted to formalize it and move the old microservices to this new paradigm....","categories": ["Domain Driven Design"],
+        "tags": ["Agile","Continuous Improvement","DDD","Team"],
+        "url": "http://localhost:4000/2017/12/10/learning-ddd-as-a-team/",
+        "teaser":null},{
+        "title": "When Cake meets NDepend",
+        "excerpt":"As a developer, I'm a big fan of automation. I can't create software where I need to push buttons and run manual scripts, to test &amp; deploy it. To help me automate my software development tasks, I use Cake, \"a cross platform build automation system with a C# DSL\". Working...","categories": ["Code Development","Continuous Integration","OSS"],
+        "tags": [".NET","Agile","Applications","Cake","Continuous Improvement","Deployment","Linux","NDepend","Windows"],
+        "url": "http://localhost:4000/2017/12/31/when-cake-meets-ndepend/",
+        "teaser":null},{
+        "title": "Migrate Magento from on-premises to the cloud",
+        "excerpt":"Last summer a good friend of mine challenged me to help him move his Magento webshop from an on-premises server to the cloud. Also, to upgrade the webshop to the newest version (Magento 2). The starting point The webshop is the online spot for a small streetwear Portuguese shop. The...","categories": ["Cloud Computing"],
+        "tags": ["AWS","Magento","Magento2"],
+        "url": "http://localhost:4000/2018/01/14/migrate-magento-from-on-premises-to-the-cloud/",
+        "teaser":null},{
+        "title": "How to solve Magento 2 upgrade errors",
+        "excerpt":"Magento 2 is the leading Open Source Software for e-commerce, with a vibrant community and proper product documentation. The development team releases bug fixes and new features in a regular cadence, given support to security threats, market trends, amongst others. Environment The environment is deployed in AWS, using an immutable...","categories": ["Continuous Deployment","Continuous Integration","Magento 2","OSS"],
+        "tags": ["Linux","Magento2"],
+        "url": "http://localhost:4000/2018/03/11/how-to-solve-magento-2-upgrade-errors/",
+        "teaser":null},{
+        "title": "Life of a C# Developer: How to build and test an AWS Lambda locally",
+        "excerpt":"Today Serverless is a thing. Although everyone can write a blog post about how Serverless run on servers, I share the same visions as Mathias Verraes: https://twitter.com/mathiasverraes/status/995317295072382976 Given that, I decided to share my developer experience building, testing and deploying AWS Lambda functions in the .NET world. Not a \"Hello...","categories": ["Cloud Computing","Continuous Deployment","OSS"],
+        "tags": [".NET Core","Agile","AWS","AWS Lambda","Continuous Improvement","Linux","Testing","Windows"],
+        "url": "http://localhost:4000/2018/05/21/life-of-a-c-developer-how-to-build-and-test-an-aws-lambda-locally/",
+        "teaser":null},{
+        "title": "What I learned from live coding during talks",
+        "excerpt":"Last week, Kenny Baas and I, delivered 2 talks with live coding. We were at NextBuild (with From EventStorming to CoDDDing) and Techorama NL (with Improving design and code quality with Property-based testing). [caption id=\"attachment_1874\" align=\"alignnone\" width=\"3456\"] Conference badges[/caption] In both talks, we used live coding as a tool to...","categories": ["Conferences","Speaking"],
+        "tags": ["Live coding","Public speaking","Soft skills"],
+        "url": "http://localhost:4000/2018/10/07/what-i-learned-from-live-coding-during-talks/",
+        "teaser":null},{
+        "title": "Using Flyway and GitLab to deploy a MySQL database to AWS RDS securely",
+        "excerpt":"One of my passions is to be a trainer. Sharing knowledge, meeting new people, be continuously challenged, it fuels my brain, and I'm always learning something new. I'm creating a new training for software development teams, and one of the components is a MySQL database. Also, I'm using a public...","categories": ["Cloud Computing","Continuous Deployment","Continuous Integration","OSS"],
+        "tags": ["AWS","CI/CD","Flyway","GitLab","MySQL","RDS"],
+        "url": "http://localhost:4000/2019/01/13/using-flyway-and-gitlab-to-deploy-a-mysql-database-to-aws-rds-securely/",
+        "teaser":null},{
+        "title": "Build and secure containers to support your CI/CD pipeline",
+        "excerpt":"There are 2 systems in any company that are critical: the payroll system, and the CI/CD system. Why? You may ask... If the payroll system doesn't work, people will leave the company and the company (may) face legal problems; the CI/CD system is the gateway to production. If it is...","categories": ["Continuous Deployment","Continuous Integration","OSS","Security"],
+        "tags": ["CI/CD","Docker","Google Container Structure Tests","Snyk","Testing"],
+        "url": "http://localhost:4000/2019/01/24/build-and-secure-containers-to-support-your-ci-cd-pipeline/",
         "teaser":null}]
