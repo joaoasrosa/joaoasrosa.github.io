9d6fb58e7177095946ea32aa844864c4b56279c1
João Rosa <joaoasrosa@gmail.com>
2019-04-27 14:12:19 +0200

    Change theme

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "Test",
+        "excerpt":"Why  Some text.   Italics   BOLD  ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/2017/08/24/test.html",
+        "teaser":null}]
